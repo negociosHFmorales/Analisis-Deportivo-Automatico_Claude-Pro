1d7d8c9ea3e363808c5299e526718455b62b5818
negociosHFmorales <negocios.HFmorales@gmail.com>
2025-08-18 07:44:59 -0600

    Create analizar_partidos.py

---
+++ analizar_partidos.py
@@ -0,0 +1,22 @@
+# Este es nuestro analizador de partidos deportivos
+# Piensa en esto como las instrucciones que le damos a la computadora
+
+import json  # Esto nos ayuda a leer los datos que vienen de las APIs
+import math  # Esto nos ayuda con las matemáticas del análisis
+
+def calcular_probabilidad_poisson(goles_esperados_local, goles_esperados_visitante):
+    """
+    Esta función es como una calculadora especial que nos dice
+    qué tan probable es que un equipo gane basándose en cuántos
+    goles esperamos que anote
+    """
+    # Aquí haremos los cálculos (los completaremos juntos)
+    pass
+
+def analizar_partido(datos_partido, datos_probabilidades):
+    """
+    Esta función toma un partido y sus probabilidades
+    y nos dice si es una buena apuesta o no
+    """
+    # Aquí analizaremos si vale la pena apostar
+    pass
